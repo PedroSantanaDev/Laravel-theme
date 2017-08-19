7cb93dcefdb8834a6d7405ab4a1d0477f100769a
Pedro <pedrosantanait@gmail.com>
2017-08-19 13:05:46 -0400

    Created theme views

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    @include('includes.head')
+</head>
+<body>
+<header class="row">
+        @include('includes.header')
+    </header>
+<div class="container">
+
+    
+
+    <div id="main" class="row">
+
+            @yield('content')
+
+    </div>
+
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+
+</div>
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+    @include('includes.head')
+</head>
+<body>
+<header class="row">
+        @include('includes.header')
+    </header>
+<div class="container">
+
+    
+
+    <div id="main" class="row">
+
+        <!-- sidebar content -->
+        <div id="sidebar" class="col-md-4">
+            @include('includes.sidebar')
+        </div>
+
+        <!-- main content -->
+        <div id="content" class="col-md-8">
+            @yield('content')
+        </div>
+
+    </div>
+
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+
+</div>
+</body>
+</html>
